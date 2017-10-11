0e0c11efafb9211d6f70dd509904a1cd0bd651cd
alexiosl1335 <alexioslgapps@Alexioss-MacBook-Pro.local>
2017-10-11 17:43:58 -0400

    Initial Commit

---
+++ OARS_mobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OARS_mobile
+//
+//  Created by Alexios Ladikos on 10/11/17.
+//  Copyright © 2017 q21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
