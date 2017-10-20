e8fee2d10592f7757653c4a877ffe0b00886322b
alexiosl1335 <alexioslgapps@Alexioss-MacBook-Pro.local>
2017-10-20 14:44:55 -0400

    profile page camera acess

--- OARS_mobile/PictureUpload.swift
+++ OARS_mobile/PictureUpload.swift
@@ -14,7 +14,11 @@ class PictureUpload: UIViewController, UINavigationControllerDelegate, UIImagePi
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        myImage.layer.borderWidth = 1
+        myImage.layer.masksToBounds = false
+//        myImage.layer.borderColor = UIColor.blackColor().CGColor
+        myImage.layer.cornerRadius = myImage.frame.height/2
+        myImage.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
@@ -22,36 +26,38 @@ class PictureUpload: UIViewController, UINavigationControllerDelegate, UIImagePi
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-//    @IBAction func importmage(_ sender: AnyObject){
-//        let image = UIImagePickerController()
-//        image.delegate = self
-//        image.sourceType = UIImagePickerControllerSourceType.photoLibrary
-//        image.allowsEditing = false
-//        self.present(image, animated: true){
-//            
-//        }
-//        
-//    }
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//        if let image = info[UIImagePickerControllerOriginalImage] as?UIImage{
-//            myImage.image=image
-//        }else{
-//           //error message
-//        }
-//        self.dismiss(animated: true, completion: nil) 
-//            
-//        
-//    }
+    @IBAction func chooseImage(_ sender: Any) {
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.delegate = self
+        
+        
+        let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: . actionSheet)
+        actionSheet.addAction(UIAlertAction(title:"Camera", style: .default, handler:{(action:UIAlertAction) in
+            if(UIImagePickerController.isSourceTypeAvailable(.camera)){
+                imagePickerController.sourceType = .camera
+                self.present(imagePickerController, animated: true, completion: nil)
+            }else{
+                print("camera not available")
+            }
+        }))
+        actionSheet.addAction(UIAlertAction(title:"Photo Library", style: .default, handler:{(action:UIAlertAction) in imagePickerController.sourceType = .photoLibrary
+            self.present(imagePickerController, animated: true, completion: nil)
 
-    /*
-    // MARK: - Navigation
+        }))
+        actionSheet.addAction(UIAlertAction(title:"Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        myImage.image = image
+        picker.dismiss(animated: true, completion: nil)
+            
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+       
+    }
 }
