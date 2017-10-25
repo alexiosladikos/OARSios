2023a179ec2352491fbc2926f1fa8a391967fcdd
alexiosl1335 <alexioslgapps@Alexioss-MacBook-Pro.local>
2017-10-25 15:44:24 -0400

    help pls

---
+++ OARS_mobile/LoginController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.swift
+//  OARS_mobile
+//
+//  Created by Alexios Ladikos on 10/16/17.
+//  Copyright © 2017 q21. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+class LoginController: UIViewController {
+
+    
+    @IBAction func action(_ sender: Any)
+    {
+        let context:LAContext = LAContext();
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error:nil){
+            
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "We need your fingerprint", reply: {
+                (wasSuccessful, error) in
+                if (wasSuccessful){
+                    print("Authentication Successful")
+                }else{
+                    print("Authentication Unsuccessful")
+                }
+            })
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
